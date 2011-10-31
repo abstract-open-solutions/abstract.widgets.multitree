92babc8f6a09d17aaf7dac6ace016602a5e55bc8
Cippino <cippinofg@gmail.com>
2011-10-31 15:42:42 +0100

    Removed internal dictionary from the page

--- abstract/widgets/multitree/browser.py
+++ abstract/widgets/multitree/browser.py
@@ -1,26 +1,26 @@
 from Products.Five import BrowserView
 import simplejson as json
 
+from abstract.widgets.multitree.widget import MultiTreeWidget
+
 class MultiTreeJSONView(BrowserView):
   
-    def _get_data(self,qry):
-        alls=[{"id":"10","name":"pippo"},
-             {"id":"20","name":"kitty"},
-             {"id":"30","name":"zoe"},
-             {"id":"40","name":"pippa"}]
-        
-        res=[]
-        for row in alls:
-            if qry in row['name']:
-                res.append(row)
-
+    def _get_data(self,qry,alls):
+        res=[dict(id=k,name=v) for k,v in alls.items() if qry in v]
         return res
     
     def __call__(self):
         self.request.response.setHeader("Content-type","application/json")
+        result_list=[]
         
-        result_list=self._get_data(self.request['q'])
-        
+        qry=self.request['q']
+        fieldName=self.request.get('field_name','')
+        if len(fieldName)>0:
+            schema=self.context.Schema()
+            field=schema.get(fieldName)
+            if field is not None:
+                vocab = field.vocabulary
+                result_list=self._get_data(qry,vocab)
         return json.dumps(result_list)
         
 
